495aea10ee9d01623d94cf6933ac86646d5e2560
xueqing <haijuanchen.sun@gmail.com>
2021-08-07 18:11:50 +0800

    Create main.go

---
+++ exercise/finalizer/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+type B struct {
+	n int
+}
+
+func NewB(n int) *B {
+	b := &B{
+		n: n,
+	}
+	runtime.SetFinalizer(b, func(pB *B) {
+		fmt.Printf("release B(%v)\n", pB.n)
+	})
+	return b
+}
+
+type A struct {
+	ptr *B
+}
+
+func NewA(b *B) *A {
+	a := &A{
+		ptr: b,
+	}
+	runtime.SetFinalizer(a, func(pA *A) {
+		fmt.Printf("release A(%v)\n", pA.ptr)
+	})
+	return a
+}
+
+func test() {
+	b := NewB(555)
+	fmt.Printf("NewB: %v\n", b)
+	a := NewA(b)
+	fmt.Printf("NewA: %v\n", a)
+}
+
+func main() {
+	test()
+	runtime.GC()
+	// if main exit here, it may not run A and B's Finalizer
+	for i := 0; i < 5; i++ {
+		time.Sleep(time.Second)
+		runtime.GC()
+	}
+	// output:
+	// 		NewB: &{555}
+	// 		NewA: &{0xc0000140e0}
+	// 		release A(&{555})
+	// 		release B(555)
+}
